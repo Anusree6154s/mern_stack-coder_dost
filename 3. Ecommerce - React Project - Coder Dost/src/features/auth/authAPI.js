72cbf0f60f12effa7bce8e86e195442b2ac1bc09
Anusree <anilkumaranusree113@gmail.com>
2024-04-15 13:01:23 +0530

    build

--- 3. Ecommerce - React Project - Coder Dost/src/features/auth/authAPI.js
+++ 3. Ecommerce - React Project - Coder Dost/src/features/auth/authAPI.js
@@ -19,6 +19,7 @@ export function LoginUser(loginInfo) {
     const response = await fetch('http://localhost:8080/auth/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
+      credentials: "include",
       headers: { 'content-type': 'application/json' }
     })
     if (response.ok) {
